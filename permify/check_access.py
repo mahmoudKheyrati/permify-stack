faae53a316939d3ab58ba2df3fa6d071a646dd46
yasin rezaei <yasinrezaei@hotmail.com>
2023-08-04 20:12:43 +0330

    some change

--- permify/check_access.py
+++ permify/check_access.py
@@ -4,27 +4,25 @@ import requests
 import json
 import requests
 
-url = "http://localhost:3476/v1/tenants/t1/permissions/check"
+url = "http://localhost:3476/v1/tenants/t2/permissions/check"
 headers = {"Content-Type": "application/json"}
 
 payload = {
-  "metadata": {
-    "schema_version": "civ7dopb0mjs73aupu10",
-    "snap_token": "KyOVrI3PdBc=",
-    "depth": 20
-  },
-  "entity": {
-    "type": "organization",
-    "id": "1"
-  },
-  "permission": "view_files",
-  "subject": {
-    "type": "user",
-    "id": "1",
-    "relation": ""
-  }
+    "metadata": {
+        "snap_token": "qFwGj5MweBc=",
+        "schema_version": "cj6fk88ochac73dd3ptg",
+        "depth": 100
+    },
+    "entity": {
+        "type": "post",
+        "id": "p1"
+    },
+    "permission": "view_post",
+    "subject": {
+        "type": "user",
+        "id": "u1"
+    }
 }
-
 response = requests.post(url, headers=headers, data=json.dumps(payload))
 
 if response.status_code == 200:
